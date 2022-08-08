acf9b34be32e0288fb27e0b406e2cb4462271d02
sergioburigana <buriganasergio@gmail.com>
2022-08-07 23:11:41 -0400

    Database update

--- main.py
+++ main.py
@@ -10,18 +10,19 @@ from wtforms import StringField, SubmitField, PasswordField
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
+import os
 
 
 
 from flask_login import UserMixin, login_user, login_required, current_user, logout_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
